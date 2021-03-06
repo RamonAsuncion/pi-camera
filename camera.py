c48193c0eb486e80736a4faae5e4a7b3fb1d2b64
Ramon Asuncion <asuncionbatista@gmail.com>
2021-03-06 10:34:15 -0500

    Reorganized code into try/except method and fixed freezing on preview

--- camera.py
+++ camera.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from gpiozero import Button
+from time import sleep
 import picamera
-import time
 
 
 def camera_module():
@@ -16,26 +16,25 @@ if __name__ == 'main':
     main()
 
 
+# Main code
 button = Button(14)
-pi_camera = picamera.PiCamera()
-#pi_camera.resolution = (x, y)
 running = True
 
-timestamp = datetime.now()
 
-
-def picture():
+def timestamp():
     # Take image and export it
+    timestamp = datetime.now()
     pi_camera.capture('picture{}.jpg').format(timestamp)
 
 
-pi_camera.start_preview()
-button.when_pressed = picture
-
 try:
     while running:
-        print('Camera is active.')
-        time.sleep(1)
+        pi_camera = camera.PiCamera()
+        #pi_camera.resolution = (x, y)
+        pi_camera.start_preview()
+        button.when_pressed = picture(pi_camera)
+        pi_camera.stop_preview()
+        sleep(1)
 except KeyboardInterrupt:  # Ctrl + C
     pi_camera.stop_preview()
     running = False
