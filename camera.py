a59ce37bbd7e13829a22435779353dd42c867069
Ramon Asuncion <asuncionbatista@gmail.com>
2021-03-08 23:32:33 -0500

    Added more more comments to the camera.py file.

--- camera.py
+++ camera.py
@@ -3,25 +3,26 @@ from gpiozero import Button
 from time import sleep
 import picamera
 
-# Main code
+# Connected to GPIO14 pin on the rasberry pi
 button = Button(14)
 running = True
 
 
 def timestamp():
-    # Take image and export it
+    # Gets the date and time.
     timestamp = datetime.now()
-    pi_camera.capture('picture{}.jpg').format(timestamp)
+    # Saves the file name as "picture + (date and time) + png"
+    pi_camera.capture('picture{}.png').format(timestamp)
 
 
 try:
     while running:
         pi_camera = camera.PiCamera()
-        #pi_camera.resolution = (x, y)
+        # pi_camera.resolution = (x, y) Resolution to be a specific width and height.
         pi_camera.start_preview()
         button.when_pressed = timestamp(pi_camera)
         pi_camera.stop_preview()
         sleep(1)
-except KeyboardInterrupt:  # Ctrl + C
+except KeyboardInterrupt:  # Ctrl + C to stop the code from running.
     pi_camera.stop_preview()
     running = False
